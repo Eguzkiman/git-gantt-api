71b058dcf7c00174cf8f69168b78f09b9fe1eb44
Eguzkiman <eguzki@kioru.com>
2019-03-27 14:03:38 -0600

    hotfix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const app = express();
 
 app.get('/exchange_github_code', (req, res) => {
-	let isOk = GITHUB_CLIENT_ID && GITHUB_CLIENT_SECRET
+	let isOk = !!GITHUB_CLIENT_ID && !!GITHUB_CLIENT_SECRET
 	return res.json({ isOk });
 });
 
