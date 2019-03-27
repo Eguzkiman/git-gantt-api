ab1ed5229bea052ec20f60768fcc6af1b1a5c84d
Eguzkiman <eguzki@kioru.com>
2019-03-27 14:01:46 -0600

    now reads config vars

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const PORT = process.env.PORT || 1337;
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 
 const app = express();
 
 app.get('/exchange_github_code', (req, res) => {
-	return res.json({
-		lol: 'okay'
-	});
+	let isOk = GITHUB_CLIENT_ID && GITHUB_CLIENT_SECRET
+	return res.json({ isOk });
 });
 
 app.listen(PORT, console.log('Git Gantt server running on port', PORT));
